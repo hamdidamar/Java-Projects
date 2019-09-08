2a726283c9f82fe88e84f47f6243fe946edee2a0
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-08 13:16:37 +0300

    Update AccountDAL.java

--- SatisVeStok/SatisVeStok/src/tr/software/hamdidamar/dataaccesslayer/AccountDAL.java
+++ SatisVeStok/SatisVeStok/src/tr/software/hamdidamar/dataaccesslayer/AccountDAL.java
@@ -5,7 +5,14 @@
  */
 package tr.software.hamdidamar.dataaccesslayer;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import tr.software.hamdidamar.core.ObjectHelper;
 import tr.software.hamdidamar.interfaces.dataaccesslayerInterfaces;
 
@@ -17,12 +24,39 @@ public class AccountDAL<AccountContract> extends ObjectHelper implements dataacc
 
     @Override
     public void Insert(AccountContract entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO Tbl_Account(AccountYetkiId,AccountPersonelId,AccountSifre))"
+                    + "VALUES("+entity.getAccountYetkiId()+","+entity.getAccountPersonelId()+",'"+entity.getAccountSifre()+"')");
+                    statement.close();
+                    connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAL.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public List<AccountContract> GetAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<AccountContract> dataAccountcontracts = new ArrayList<AccountContract>();
+        
+        Connection connection = getConnection();
+        AccountContract contract;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Tbl_Kategori");
+            while (resultSet.next()) {
+                contract = new AccountContract();
+                contract.setAccountId(resultSet.getInt("AccountId"));
+                contract.getAccountYetkiId(resultSet.getInt("AccountYetkiId"));
+                contract.getAccountPersonelId(resultSet.getString("AccountPersonelId"));
+                contract.getAccountSifre(resultSet.getString("AccountSifre"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return dataAccountcontracts;
     }
 
     @Override
