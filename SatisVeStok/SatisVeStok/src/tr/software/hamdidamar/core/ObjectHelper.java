1ddf020c821e57ba40faab46f0bd286213164c21
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-09-10 00:23:45 +0300

    Update ObjectHelper.java

--- SatisVeStok/SatisVeStok/src/tr/software/hamdidamar/core/ObjectHelper.java
+++ SatisVeStok/SatisVeStok/src/tr/software/hamdidamar/core/ObjectHelper.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tr.software.hamdidamar.core;
 
 import java.sql.Connection;
@@ -11,34 +6,28 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import tr.software.hamdidamar.interfaces.coreInterfaces;
+import java.sql.*;
 
-/**
- *
- * @author hmdmr
- */
 public class ObjectHelper extends CoreFields implements coreInterfaces{
 
-   
     static{
         try {
-            Class.forName("com.mysql.jdbo.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ObjectHelper.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Bağlantı Driver Hatası");
         }
     }
     
-    
+    @Override
     public Connection getConnection() {
         Connection connection = null;
         
         try {
             connection = DriverManager.getConnection(getUrl(),getUserName(),getPassword());
         } catch (SQLException ex) {
-            Logger.getLogger(ObjectHelper.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Bağlantı Hatası...");
         }
         return connection;
     }
 
-    
-    
 }
